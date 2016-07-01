19f35934c43d85c293521f331cafa2baded2ee21
Shen Chauhan <shenchauhan@hotmail.com>
2016-07-01 14:47:02 -0700

    Hunt the Wumpus Direct Line demo

--- HuntTheWumpus/App.xaml.cs
+++ HuntTheWumpus/App.xaml.cs
@@ -28,6 +28,9 @@ namespace HuntTheWumpus
         /// </summary>
         public App()
         {
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
@@ -42,7 +45,7 @@ namespace HuntTheWumpus
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                //this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
             Frame rootFrame = Window.Current.Content as Frame;
--- HuntTheWumpus/MainPage.xaml.cs
+++ HuntTheWumpus/MainPage.xaml.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using Microsoft.Bot.Connector.DirectLine;
+using Microsoft.Bot.Connector.DirectLine.Models;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace HuntTheWumpus
 {
@@ -25,6 +17,45 @@ namespace HuntTheWumpus
         public MainPage()
         {
             this.InitializeComponent();
+            KeyDown += (s, e) =>
+            {
+                if (e.Key == Windows.System.VirtualKey.Enter)
+                {
+                    SendButton_Click(s, e);
+                }
+            };
+        }
+
+        public ObservableCollection<Message> Messages { get; set; } = new ObservableCollection<Message>();
+
+        private DirectLineClient _directLine;
+        private string _conversationId;
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            _directLine = new DirectLineClient("xkzv4Vz_910.cwA.y3w.Y1bblOqVLBqf6mJDvGejaIvI6i2QhjF7I1-ZadnJpo4");
+            var conversation = await _directLine.Conversations.NewConversationWithHttpMessagesAsync();
+            _conversationId = conversation.Body.ConversationId;
+        }
+
+        private async Task GetMessages()
+        {
+            var httpMessages = await _directLine.Conversations.GetMessagesWithHttpMessagesAsync(_conversationId);
+            var messages = httpMessages.Body.Messages;
+
+            foreach (var message in messages.Where(httpMessage => !Messages.Any(existingMessage => existingMessage.Id == httpMessage.Id)))
+            {
+                Messages.Add(message);
+            }
+        }
+
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            var message = MessageToSendTextBox.Text;
+            MessageToSendTextBox.Text = string.Empty;
+
+            await _directLine.Conversations.PostMessageAsync(_conversationId, new Message { Text = message });
+            await GetMessages();
         }
     }
 }
